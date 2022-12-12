26efaad755821a9392792b5df57515dc9f9b7fcd
yandrade1305 <48693812+yandrade1305@users.noreply.github.com>
2022-12-12 17:36:32 -0300

    Subindo projeto

---
+++ tp1/src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class MainTest {
+    @Test
+    public void test(){
+        assertEquals("Olá mundo", "Olá mundo");
+    }
+}
