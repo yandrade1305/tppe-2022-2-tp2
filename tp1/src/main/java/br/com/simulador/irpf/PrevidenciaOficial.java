7a26a7e72d4e464bb11a20231e74cb14ae11b10e
Abner Filipe <abner.f.c.r@hotmail.com>
2022-12-20 01:25:39 -0300

    [refac] Corrigindo heranca de deducao em pensao alimenticia e previdencia oficial

--- tp1/src/main/java/br/com/simulador/irpf/PrevidenciaOficial.java
+++ tp1/src/main/java/br/com/simulador/irpf/PrevidenciaOficial.java
@@ -1,12 +1,8 @@
 package br.com.simulador.irpf;
 
 public class PrevidenciaOficial extends Deducao {
-    private String descricao;
-    private float valor;
 
     public PrevidenciaOficial(String descricao, float valor) {
         super(descricao, valor);
-        this.descricao = descricao;
-        this.valor = valor;
     }
 }
