eaa7d842cfb1f9e8c0ad1e3f229c34d060f47895
Abner Filipe <abner.f.c.r@hotmail.com>
2022-12-20 00:47:54 -0300

    [Red] Implementando fase red TDD, cadastro de previdencia oficial

---
+++ tp1/src/main/java/br/com/simulador/irpf/PrevidenciaOficial.java
@@ -0,0 +1,11 @@
+package br.com.simulador.irpf;
+
+public class PrevidenciaOficial {
+    private String descricao;
+    private float valor;
+
+    public PrevidenciaOficial(String descricao, float valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+}
---
+++ tp1/src/test/java/br/com/simulador/irpf/CadastroPrevidenciaOficialTest.java
@@ -0,0 +1,21 @@
+package br.com.simulador.irpf;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class CadastroPrevidenciaOficialTest {
+    SimuladorIRPF simulador;
+
+    @Before
+    public void setup() {
+        simulador = new SimuladorIRPF();
+    }
+
+    @Test
+    public void deveCadastrarUmaPrevidenciaOficial() {
+        simulador.cadastrarPrevidenciaOficial(198.50f);
+        assertEquals(198.50f, simulador.getTotalDeducoes(), 0F);
+    }
+}
