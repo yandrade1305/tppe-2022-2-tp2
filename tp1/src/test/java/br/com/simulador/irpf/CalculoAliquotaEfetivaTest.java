1828e371be60ef3daf890182cbc191550c8a0b8c
Abner Filipe <abner.f.c.r@hotmail.com>
2022-12-21 03:31:34 -0300

    [Red] Fase red do TDD, implementando teste para calcular aliquota efetiva

---
+++ tp1/src/test/java/br/com/simulador/irpf/CalculoAliquotaEfetivaTest.java
@@ -0,0 +1,23 @@
+package br.com.simulador.irpf;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculoAliquotaEfetivaTest {
+
+    SimuladorIRPF simulador;
+
+    @Before
+    public void setup() {
+        simulador = new SimuladorIRPF();
+    }
+
+    @Test
+    public void deveCalcularAliquotaEfetivaComUmRendimento() {
+        simulador.cadastrarUmRendimento("Salario", 3000f);
+        float aliquotaEfetiva = simulador.calcularAliquotaEfetiva();
+        assertEquals(3.17, aliquotaEfetiva);
+    }
+}
