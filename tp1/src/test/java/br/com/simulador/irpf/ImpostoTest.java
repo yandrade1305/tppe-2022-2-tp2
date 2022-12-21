f4330361de613159d59f7821bf3156716b05332c
Denys Rógeres <dennysrogeres@gmail.com>
2022-12-21 00:46:27 -0300

    adicionando testes duplicação para impostos

--- tp1/src/test/java/br/com/simulador/irpf/ImpostoTest.java
+++ tp1/src/test/java/br/com/simulador/irpf/ImpostoTest.java
@@ -37,46 +37,103 @@ public class ImpostoTest {
         
     }
 
-    @Test
-    public void deveRetornarTotalImpostoFaixa1Falsificado() {
-        simulador.cadastrarUmRendimento("Salário", 1300F);
-        assertEquals(0F, simulador.getImposto(), 0F);
-    }
+//    @Test
+//    public void deveRetornarTotalImpostoFaixa1Falsificado() {
+//        simulador.cadastrarUmRendimento("Salário", 1300F);
+//        assertEquals(0F, simulador.getImposto(), 0F);
+//    }
+//    
+//    @Test
+//    public void deveRetornarTotalImpostosFaixa2Falsificado() {
+//    	simulador.cadastrarUmRendimento("Salário", 2600);
+//    	assertEquals(52.2015F, simulador.getImposto(), 0F);
+//    }
+//    
+//    @Test
+//    public void deveRetornarTotalImpostosFaixa3Falsificado() {
+//    	simulador.cadastrarUmRendimento("Salário", 3500);
+//    	assertEquals(170.20276F, simulador.getImposto(), 0F);
+//    }
+//
+//    @Test
+//    public void deveRetornarTotalImpostosFaixa4Falsificado() {
+//    	simulador.cadastrarUmRendimento("Salário", 4000);
+//    	assertEquals(263.874F, simulador.getImposto(), 0F);
+//    }
+//    
+//    @Test
+//    public void deveRetornarTotalImpostosFaixa5Falsificado() {
+//    	simulador.cadastrarUmRendimento("Salário", 5000);
+//    	assertEquals(505.64F, simulador.getImposto(), 0F);
+//    }
     
-    @Test
-    public void deveRetornarTotalImpostosFaixa2() {
-    	simulador.cadastrarUmRendimento("Salário", 2600);
-    	assertEquals(52.2015F, simulador.getImposto(), 0F);
-    }
-    
-    @Test
-    public void deveRetornarTotalImpostosFaixa3() {
-    	simulador.cadastrarUmRendimento("Salário", 3500);
-    	assertEquals(170.20276F, simulador.getImposto(), 0F);
-    }
-
-    @Test
-    public void deveRetornarTotalImpostosFaixa4() {
-    	simulador.cadastrarUmRendimento("Salário", 4000);
-    	assertEquals(263.874F, simulador.getImposto(), 0F);
-    }
-    
-    @Test
-    public void deveRetornarTotalImpostosFaixa5() {
-    	simulador.cadastrarUmRendimento("Salário", 5000);
-    	assertEquals(505.64F, simulador.getImposto(), 0F);
-    }
     @Test
     public void deveRetornarTotalImpostoFaixa1Duplicado() {
 
         simulador.cadastrarUmRendimento("Salário", 300F);
         assertEquals(0F, simulador.getImposto(), 0F);
 
-        simulador.cadastrarUmRendimento("Salário", 1000F);
+        simulador.cadastrarUmRendimento("Liquidação de ações", 1000F);
         assertEquals(0F, simulador.getImposto(), 0F);
 
-        simulador.cadastrarUmRendimento("Salário", 500F);
+        simulador.cadastrarUmRendimento("Renda Fixa", 500F);
         assertEquals(0F, simulador.getImposto(), 0F);
+
+    }
+    
+    @Test
+    public void deveRetornarTotalImpostoFaixa2Duplicado() {
+
+        simulador.cadastrarUmRendimento("Salário", 2000F);
+        assertEquals(7.20F, simulador.getImposto(), 2F);
+
+        simulador.cadastrarUmRendimento("Renda Fixa", 200F);
+        assertEquals(22.20F, simulador.getImposto(), 2F);
+
+        simulador.cadastrarUmRendimento("Liquidação de ações", 600F);
+        assertEquals(67.20F, simulador.getImposto(), 2F);
+
+    }
+    
+    @Test
+    public void deveRetornarTotalImpostoFaixa3Duplicado() {
+
+        simulador.cadastrarUmRendimento("Salário", 3000F);
+        assertEquals(95.20F, simulador.getImposto(), 2F);
+
+        simulador.cadastrarUmRendimento("Renda Fixa", 200F);
+        assertEquals(125.20F, simulador.getImposto(), 2F);
+
+        simulador.cadastrarUmRendimento("Liquidação de ações", 250F);
+        assertEquals(162.70F, simulador.getImposto(), 2F);
+
+    }
+    
+    @Test
+    public void deveRetornarTotalImpostoFaixa4Duplicado() {
+
+        simulador.cadastrarUmRendimento("Salário", 4000F);
+        assertEquals(263.87F, simulador.getImposto(), 2F);
+
+        simulador.cadastrarUmRendimento("Renda Fixa", 350F);
+        assertEquals(342.62F, simulador.getImposto(), 2F);
+
+        simulador.cadastrarUmRendimento("Liquidação de ações", 50F);
+        assertEquals(353.87F, simulador.getImposto(), 2F);
+
+    }
+    
+    @Test
+    public void deveRetornarTotalImpostoFaixa5Duplicado() {
+
+        simulador.cadastrarUmRendimento("Salário", 4800F);
+        assertEquals(450.64F, simulador.getImposto(), 2F);
+
+        simulador.cadastrarUmRendimento("Renda Fixa", 800F);
+        assertEquals(670.64F, simulador.getImposto(), 2F);
+
+        simulador.cadastrarUmRendimento("Liquidação de ações", 400F);
+        assertEquals(780.64F, simulador.getImposto(), 2F);
 
     }
     
